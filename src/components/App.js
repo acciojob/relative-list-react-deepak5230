6dc26230e65f25682c8837919acbe754c6f4e5bf
deepak <deepakgupta351@gmail.com>
2024-08-23 17:21:39 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
+const DiwaliRelatives = () => {
+  // List of relatives
+  const relatives = [
+    'Aunt Rita',
+    'Uncle Raj',
+    'Cousin Priya',
+    'Grandma Sita',
+    'Grandpa Krishna'
+  ];
+
   return (
-    <div id="main">
-               {/* Do not remove the main div */}
+    <div>
+      <h1>Diwali Visits</h1>
+      <ol key="relativeList">
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>
+            {relative}
+          </li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default DiwaliRelatives;
